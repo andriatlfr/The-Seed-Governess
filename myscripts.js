6fbd963fdf44a27e30a7d42adb90e1f2d67ad5f5
andriatlfr <119631108+andriatlfr@users.noreply.github.com>
2024-03-28 14:48:53 -0600

    java script hidden js file

--- myscripts.js
+++ myscripts.js
@@ -6,3 +6,30 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  //Intersection Observer Function - looks for intersection of where the content is in relation to the viewport. 
+//IntersectionObserver Function
+const observer = new IntersectionObserver (entries => {
+
+  entries.forEach ( (entry) => {
+
+      if (entry.isIntersecting) {
+
+          entry.target.classList.add("show");
+
+      } else {
+
+          entry.target.classList.remove("show");
+
+      }
+  }
+
+  )
+
+}
+
+);
+//establishing variable, name of variable, establish parameters,for each entry in entries, => means shorthand entry
+//Target animated elements
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach(el => observer.observe(el));
