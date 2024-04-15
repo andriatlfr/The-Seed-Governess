9be2db8f2bfc6b5b0cd792585f80f9a187d41dcb
andriatlfr <119631108+andriatlfr@users.noreply.github.com>
2024-04-15 17:08:00 -0600

    deleted unnecessary java

--- js/myscripts.js
+++ js/myscripts.js
@@ -57,12 +57,5 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+
 
